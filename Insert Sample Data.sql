b8413fe44e63d3111f59cd8278ab3e683733827d
darshanpatil2511 <patil.da@northeastern.edu>
2025-03-23 23:40:45 -0400

    Data for UserRoles
    Data for UserRoles

---
+++ Insert Sample Data.sql
@@ -0,0 +1,9 @@
+-- DATA for DMDD PROJECT
+
+-- Insert Sample Data for Testing
+
+-- Insert Data for UserRoles
+INSERT INTO UserRoles VALUES ('R001', 'Admin');
+INSERT INTO UserRoles VALUES ('R002', 'Doctor');
+INSERT INTO UserRoles VALUES ('R003', 'BillingStaff');
+
